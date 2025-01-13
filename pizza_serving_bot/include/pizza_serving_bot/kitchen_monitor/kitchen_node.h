b0c233743d2303c6941ba0756453eb3d93912aac
Gunn Oh <pedralation@gmail.com>
2025-01-13 18:51:41 +0900

    Update kitchen_node.h

--- pizza_serving_bot/include/pizza_serving_bot/kitchen_monitor/kitchen_node.h
+++ pizza_serving_bot/include/pizza_serving_bot/kitchen_monitor/kitchen_node.h
@@ -1,5 +1,5 @@
-#ifndef KITCHEN_SERVICE_NODE_H
-#define KITCHEN_SERVICE_NODE_H
+#ifndef KITCHEN_NODE_H
+#define KITCHEN_NODE_H
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
@@ -8,13 +8,14 @@
 #include <pizza_order_msgs/msg/call_manager.hpp>
 #include <pizza_order_msgs/srv/goal_loc.hpp>
 #include <QObject>
+#include <QTimer>
 
 class KitchenServiceNode : public rclcpp::Node
 {
 public:
-    KitchenServiceNode();
+    KitchenServiceNode(QObject *parent = nullptr);
 
-    // Signal을 위한 QObject 상속 (Qt와 ROS 통신을 위해)
+    // Signal을 위한 QObject 상속
     class SignalBridge : public QObject
     {
         Q_OBJECT
@@ -25,6 +26,12 @@ public:
     };
 
     SignalBridge* getSignalBridge() { return &signal_bridge_; }
+
+    // 주문취소 서비스 클라이언트
+    rclcpp::Client<pizza_order_msgs::srv::CancelService>::SharedPtr cancel_client_;
+
+    // 주문정보와 Goal(테이블 위치) 클라이언트
+    rclcpp::Client<pizza_order_msgs::srv::GoalLoc>::SharedPtr goal_client_;
 
 private:
     // 콜백 함수
@@ -37,11 +44,9 @@ private:
 
     // 서비스 및 토픽
     rclcpp::Service<pizza_order_msgs::srv::OrderService>::SharedPtr order_service_;
-    rclcpp::Client<pizza_order_msgs::srv::CancelService>::SharedPtr cancel_client_;
     rclcpp::Subscription<pizza_order_msgs::msg::CallManager>::SharedPtr call_manager_subscriber_;
-    rclcpp::Client<pizza_order_msgs::srv::GoalLoc>::SharedPtr goal_loc_client_;
 
     SignalBridge signal_bridge_;
 };
 
-#endif // KITCHEN_SERVICE_NODE_H
+#endif  // KITCHEN_NODE_H
